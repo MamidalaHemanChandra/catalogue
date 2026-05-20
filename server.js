4f1cbce592e12643a0e32982322c77a59217d8db
heman chandra <hemanchandra701@gmail.com>
2026-05-20 23:23:55 +0530

    jenkins

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get('/health', (req, res) => {
-    var const = {
+    var let = {
         app: 'OK',
         mongo: mongoConnected
     };
